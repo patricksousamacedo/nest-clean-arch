75a6218b79e0d59f1fd920be882a3383176ce19e
patricksousamacedo <patricksouzamacedo@gmail.com>
2025-11-29 13:01:30 -0300

    config dev enviroment

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+} from '@nestjs/platform-fastify'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
-  );
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+    new FastifyAdapter(),
+  )
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 }
-bootstrap();
+bootstrap().catch((err: Error) => {
+  console.error(err)
+  process.exit(1)
+})
